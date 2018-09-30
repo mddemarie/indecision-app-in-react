ae5757f1d2342277e1913c9fcf831bfd9d40536b
mddemarie <mddemarie@gmail.com>
2018-09-30 23:12:14 +0200

    added a syntax for long and short arrow function

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,14 @@
+function square (x) {
+    return x * x;
+};
+
+console.log(square(3));
+
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+
+// Arrow function expression syntax
+const squareArrow = (x) => x * x;
+
+console.log(squareArrow(8));
