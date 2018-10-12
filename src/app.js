ea087b9ca0857fa638f9d7feb88d86e2b58e5bd3
mddemarie <mddemarie@gmail.com>
2018-10-12 17:14:00 +0200

    solved the Udemy code challenge with minusOne & reset buttons

--- src/app.js
+++ src/app.js
@@ -23,12 +23,19 @@ let count = 0;
 const addOne = () => {
   console.log('addOne');
 };
+const minusOne = () => {
+  console.log('minusOne');
+};
+const reset = () => {
+  console.log('reset');
+};
+
 const templateTwo = (
   <div>
     <h1>Count: {count}</h1>
-    <button onClick={() => {
-      console.log('some value here');
-    }}>+1</button>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>reset</button>
   </div>
 )
 
