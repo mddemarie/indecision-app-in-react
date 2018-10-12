3ebc62b847b62297ee90ad33f6c201d9ddd93f4d
mddemarie <mddemarie@gmail.com>
2018-10-12 18:13:06 +0200

    added onSubmit event & form element input

--- src/app.js
+++ src/app.js
@@ -3,7 +3,18 @@ console.log('The app.js is running!');
 const app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
-  options: ['one', 'two']
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+  }
 };
 
 // JSX - JavaScript XML
@@ -12,10 +23,15 @@ const template = (
     <h1>{app.title}</h1>
     {app.subtitle && <p>{app.subtitle}</p>}
     <p>{app.options.length > 0 ? 'Here are your options:' : 'No options'}</p>
+    <p>{app.options.length}</p>
     <ol>
-      <li>Item {app.options[0]}</li>
-      <li>Item {app.options[1]}</li>
+      <li>Item one</li>
+      <li>Item two</li>
     </ol>
+    <form onSubmit={onFormSubmit}>
+      <input type="text" name="option"/>
+      <button>Add Option</button>
+    </form>
   </div>
 );
 
