11c560acc5940ea86e71852e1922b6d56770bf82
mddemarie <mddemarie@gmail.com>
2018-10-19 18:05:30 +0200

    learnt about tests with jest

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,51 @@
+const { addExpense, editExpense, removeExpense, ALABAMA } = '../../actions/expenses';
+
+test('should remove expense action object', () => {
+    const action = removeExpense({ id: '123abc' });
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('should edit expense action object', () => {
+    const action = editExpense('123', {note: 'New note value'});
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123',
+        updates: {
+            note: 'New note value'
+        }
+    });
+});
+
+test('should add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'Rent',
+        amount: 109500,
+        createdAt: 1000,
+        note: 'This was last months rent'
+    };
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
+
+test('should add expense action object with default values', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            description: '',
+            note: '',
+            amount: 0,
+            createdAt: 0
+        }
+    });
+});
