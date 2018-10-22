d44bbed8e9a6ac41015558397b6c302d8c7eb0b4
mddemarie <mddemarie@gmail.com>
2018-10-22 17:50:49 +0200

    learnt about component props & completed code assignment

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 class IndecisionApp extends React.Component {
   render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['Thing one', 'Thing two', 'Thing four'];
+
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle}/>
         <Action />
-        <Options />
+        <Options options={options}/>
       </div>
     )
   }
@@ -14,8 +18,8 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
@@ -35,9 +39,7 @@ class Options extends React.Component {
   render() {
     return (
       <div>
-        <h3>Here are your options:</h3>
-        <Option />
-        <Option />
+        {this.props.options.map((option) => <Option key={option} optionText={option}/>)}
       </div>
     );
   }
@@ -47,7 +49,7 @@ class Option extends React.Component {
   render() {
     return (
       <div>
-        <p>Item</p>
+        <p>{this.props.optionText}</p>
       </div>
     );
   }
