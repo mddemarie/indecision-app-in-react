eea3ba37da3765b99282d3313f0c9dac88281fa5
mddemarie <mddemarie@gmail.com>
2018-10-24 17:43:47 +0200

    removed defaultProps because we have localStorage

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ class IndecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: props.options
+      options: []
     };
   }
 
@@ -88,10 +88,6 @@ class IndecisionApp extends React.Component {
     )
   }
 }
-
-IndecisionApp.defaultProps = {
-  options: []
-};
 
 const Header = (props) => {
   return (
