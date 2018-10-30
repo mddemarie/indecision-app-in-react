36d58627df41e003346db6947074c1036946a8cf
mddemarie <mddemarie@gmail.com>
2018-10-30 13:56:30 +0100

    installed React & react-dom and loading with webpack without jsx

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
-import anythinIWant, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(10, 10));
-console.log(anythinIWant(30, 10));
-
-console.log(isAdult(30));
-console.log(canDrink(21));
-console.log(isSenior(64));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
--- src/utils.js
+++ src/utils.js
@@ -7,5 +7,6 @@ export const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 
 export default substract;
-
+// export default (a, b) => a - b; this will work as well
+// export default substract = (a, b) => a - b; this won't work
 // export { square, add, substract as default };
