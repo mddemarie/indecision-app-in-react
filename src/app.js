fc7f030793ac6c9b69c1ca0108ed7f9852d832cb
mddemarie <mddemarie@gmail.com>
2018-11-16 17:12:20 +0100

    started to separate the Route components into files

--- src/app.js
+++ src/app.js
@@ -1,62 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-// import './styles/styles.scss'
+import AppRouter from './routers/AppRouter'
+// import './styles/styles.scss' this does not work yet
 
-const ExpenseDashboardPage = () =>  (
-    <div>
-        This is from my dashboard component
-    </div>
-)
-
-const AddExpensePage = () =>  (
-    <div>
-        This is from my add expense component
-    </div>
-)
-
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-)
-
-const HelpPage = () => (
-    <div>
-        This is from my help page component
-    </div>
-)
-
-const NotFoundPage = () => (
-    <div>
-        404! - <Link to="/">Go Home</Link>
-        {/* 404! - <a href="/">Go Home</a> */}
-    </div>
-)
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Home Page</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-)
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
