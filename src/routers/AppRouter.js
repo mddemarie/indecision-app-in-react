9d07663bb410f049fbd7703b87fbf99abed2d987
mddemarie <mddemarie@gmail.com>
2018-11-20 18:36:52 +0100

    break down the app into separate routers

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,46 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const ExpenseDashboardPage = () =>  (
-    <div>
-        This is from my dashboard component
-    </div>
-)
-
-const AddExpensePage = () =>  (
-    <div>
-        This is from my add expense component
-    </div>
-)
-
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-)
-
-const HelpPage = () => (
-    <div>
-        This is from my help page component
-    </div>
-)
-
-const NotFoundPage = () => (
-    <div>
-        404! - <Link to="/">Go Home</Link>
-        {/* 404! - <a href="/">Go Home</a> */}
-    </div>
-)
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Home Page</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-)
+import Header from '../components/HeaderRouter';
+import ExpenseDashboardPage from '../components/DashboardRouter';
+import AddExpensePage from '../components/AddRouter';
+import EditExpensePage from '../components/EditRouter';
+import HelpPage from '../components/HelpRouter';
+import NotFoundPage from '../components/NotFoundRouter';
 
 const AppRouter = () => (
     <BrowserRouter>
@@ -57,4 +23,4 @@ const AppRouter = () => (
     </BrowserRouter>
 );
 
-export default AppRouter
+export default AppRouter;
