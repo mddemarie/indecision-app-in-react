c9a767e4b4a0aa6c60a97a7bdcc405da3d5bd1fb
mddemarie <mddemarie@gmail.com>
2018-11-21 10:59:53 +0100

    add dynamic id in url

--- src/components/EditRouter.js
+++ src/components/EditRouter.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            This is from my edit expense component
+        </div>
+    );
+}
 
 export default EditExpensePage;
