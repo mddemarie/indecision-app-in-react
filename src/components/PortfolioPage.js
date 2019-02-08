8d6f5d3290c199ffdffdb63b941c31b9a597013e
mddemarie <mddemarie@gmail.com>
2019-02-08 18:26:09 +0100

    write all components & add NavLinks for navigation on Home page

---
+++ src/components/PortfolioPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortfolioPage = () => (
+    <div>
+        <h1>My Work</h1>
+        <p>Checkout the stuff I've done:</p>
+        <Link to="/portfolio/1">Item one</Link>
+        <Link to="/portfolio/2">Item two</Link>
+    </div>
+)
+
+export default PortfolioPage;
