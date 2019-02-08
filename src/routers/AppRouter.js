767f04f819dc172198cc6ff86c5eba8fbcb5d1ad
mddemarie <mddemarie@gmail.com>
2019-02-08 18:26:45 +0100

    add each component to the router & bundle a new app

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import NotFoundPage from '../components/NotFound';
+import HomePage from '../components/HomePage';
+import ContactPage from '../components/ContactPage';
+import PortfolioItemPage from '../components/PortfolioItemPage';
+import PortfolioPage from '../components/PortfolioPage';
 import Header from '../components/Header';
-import NotFoundPage from '../components/NotFoundRouter';
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
-            <Header />
+            <Header/>
             <Switch>
-                <Route component={NotFoundPage} />
+                <Route path="/" component={HomePage} exact={true} />
+                <Route path="portfolio" component={PortfolioPage} exact={true} />
+                <Route path="/portfolio/:id" component={PortfolioItemPage} />
+                <Route path="/contact" component={ContactPage} />
+                <Route component={NotFoundPage}/>
             </Switch>
         </div>
     </BrowserRouter>
