5248fa33c971f4497ba941ef1c52c3fd97aa8f23
mddemarie <mddemarie@gmail.com>
2019-03-01 15:05:56 +0100

    commit

--- src/components/EditRouter.js
+++ src/components/EditRouter.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-);
+const EditExpensePage = (props) => {
+    return (
+        <div>
+            This is from my edit expense component
+        </div>
+    );
+}
 
 export default EditExpensePage;
